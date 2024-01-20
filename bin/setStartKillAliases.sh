9cdfad57ed28275d8332ac39519ca06751f48da5
Morgan Sziraki <morgan.sziraki@gmail.com>
2024-01-20 18:13:47 +0000

    re org

--- bin/setStartKillAliases.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-alias killer="ps | grep -v grep | grep 'ruby app_red_green.rb' | awk '{print $1}' | xargs kill -9"
-alias starter='nohup ruby app_red_green.rb 2>&1 > app_red_green.log&'
